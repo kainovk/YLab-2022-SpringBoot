a958451bb73dd511d0231b98d663159a9053eb8d
Kirill Kainov <kainov.dev@gmail.com>
2022-09-17 12:45:29 +0300

    add entities

---
+++ src/main/java/com/edu/ulab/app/entity/UserEntity.java
@@ -0,0 +1,14 @@
+package com.edu.ulab.app.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserEntity {
+    private Long id;
+    private String fullName;
+    private String title;
+    private int age;
+    private List<BookEntity> books;
+}

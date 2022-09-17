6d3e5417747bfc914cbeff47dc85af8963f4da11
Kirill Kainov <kainov.dev@gmail.com>
2022-09-17 12:43:07 +0300

    add repository layer

---
+++ src/main/java/com/edu/ulab/app/storage/repository/CrudRepository.java
@@ -0,0 +1,16 @@
+package com.edu.ulab.app.storage.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudRepository<T, ID> {
+    T save(T entity);
+
+    Optional<T> findById(ID id);
+
+    List<T> findAll();
+
+    void deleteById(ID id);
+
+    void delete(T entity);
+}

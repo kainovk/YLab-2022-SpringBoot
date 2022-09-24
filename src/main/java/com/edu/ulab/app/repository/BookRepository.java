5a966c1bfa2972d2bd7bb77cdc48cab73e24be28
Kirill Kainov <kainov.dev@gmail.com>
2022-09-24 12:16:39 +0300

    replace in-memory storage with db repositories

---
+++ src/main/java/com/edu/ulab/app/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.edu.ulab.app.repository;
+
+import com.edu.ulab.app.entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+}

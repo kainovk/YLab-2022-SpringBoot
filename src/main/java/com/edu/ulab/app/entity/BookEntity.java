fe3b8d63a5643daf289b860504ac7538592e5f2a
Kirill Kainov <kainov.dev@gmail.com>
2022-09-24 11:56:52 +0300

    configure db

--- src/main/java/com/edu/ulab/app/entity/BookEntity.java
+++ src/main/java/com/edu/ulab/app/entity/BookEntity.java
@@ -3,9 +3,18 @@ package com.edu.ulab.app.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
-@Builder
+@Entity
+@Table(name = "BOOK")
 public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long userId;
     private String title;
--- src/main/java/com/edu/ulab/app/entity/UserEntity.java
+++ src/main/java/com/edu/ulab/app/entity/UserEntity.java
@@ -3,9 +3,18 @@ package com.edu.ulab.app.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
-@Builder
+@Entity
+@Table(name = "PERSON")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fullName;
     private String title;

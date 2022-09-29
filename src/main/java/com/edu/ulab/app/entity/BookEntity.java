54b36f40e5e93a97c3bb3af297b4bd199f3c62dc
Kirill Kainov <kainov.dev@gmail.com>
2022-09-29 15:39:05 +0300

    add constraints

--- src/main/java/com/edu/ulab/app/entity/BookEntity.java
+++ src/main/java/com/edu/ulab/app/entity/BookEntity.java
@@ -7,6 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Data
 @Entity
@@ -15,8 +18,13 @@ public class BookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private Long userId;
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @NotNull
+    @Positive
     private long pageCount;
 }
--- src/main/java/com/edu/ulab/app/entity/UserEntity.java
+++ src/main/java/com/edu/ulab/app/entity/UserEntity.java
@@ -1,12 +1,15 @@
 package com.edu.ulab.app.entity;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -15,7 +18,11 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String fullName;
+    @NotBlank
     private String title;
+    @NotNull
+    @Range(min = 0, max = 125)
     private int age;
 }
